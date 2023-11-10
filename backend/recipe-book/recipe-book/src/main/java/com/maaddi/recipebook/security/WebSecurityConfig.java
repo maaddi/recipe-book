26010c104b553299daeb1b9614602fe4f9dbce2d
maaddi <101003016+maaddi@users.noreply.github.com>
2023-11-11 00:04:37 +0100

    implement UserDetailsImpl and UserDetailsServiceImpl for receiving user details

--- backend/recipe-book/recipe-book/src/main/java/com/maaddi/recipebook/security/WebSecurityConfig.java
+++ backend/recipe-book/recipe-book/src/main/java/com/maaddi/recipebook/security/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.maaddi.recipebook.security;
 
+import com.maaddi.recipebook.security.services.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
---
+++ backend/recipe-book/recipe-book/src/main/java/com/maaddi/recipebook/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.maaddi.recipebook.security.services;
+
+import com.maaddi.recipebook.domain.entities.User;
+import com.maaddi.recipebook.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+}
