ace9c1b6efe1aaa8e6170b9ea12fc7dd2e8af1e6
maaddi <101003016+maaddi@users.noreply.github.com>
2023-11-12 19:02:40 +0100

    test commit

---
+++ backend/recipe-book/test/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int number = 3;
+        String hello = "hello";
+        int test = Integer(hello);
+        System.out.println(test);
+    }
+}
