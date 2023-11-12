24fd8cad7d02dc324bbafcc634db9a2562a58241
maaddi <101003016+maaddi@users.noreply.github.com>
2023-11-12 19:04:31 +0100

    remove unintended folder

--- backend/recipe-book/test/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        int number = 3;
-        String hello = "hello";
-        int test = Integer(hello);
-        System.out.println(test);
-    }
-}
