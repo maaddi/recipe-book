438c9f4f7b1ae14b7c10b80a6cdd46ceea49b969
maaddi <101003016+maaddi@users.noreply.github.com>
2023-11-20 22:25:07 +0100

    create services for login and register

---
+++ frontend/src/services/auth.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {LoginRequest} from "../dtos/login-request";
+import {Observable} from "rxjs";
+import {RegisterRequest} from "../dtos/register-request";
+
+const AUTH_API = 'http://localhost:8080/api/auth/';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  login(loginRequest: LoginRequest): Observable<any> {
+    const url = AUTH_API + 'signin';
+    return this.http.post(url, loginRequest, httpOptions);
+  }
+
+  register(registerRequest: RegisterRequest): Observable<any> {
+    const url = AUTH_API + 'signup';
+    return this.http.post(url, registerRequest, httpOptions);
+  }
+
+  logout(): Observable<any> {
+    const url = AUTH_API + 'signout';
+    return this.http.post(url, httpOptions);
+  }
+}
